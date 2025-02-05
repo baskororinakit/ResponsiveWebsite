105981ac68ba703163b55ac15750d63c2c920654
Baskoro Rinakit <baskororinakit03@gmail.com>
2025-02-05 21:27:37 +0700

    Adding some syntax on javascript

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ const deviceLink = document.getElementById("devices-link")
 menuIcon.addEventListener("click", () => {
     menuList.classList.toggle("hidden")
 });
+
+deviceLink.forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
