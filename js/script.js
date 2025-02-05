c8b98a9a6a16b8412da087c2f2453396b7e06a0f
Baskoro Rinakit <baskororinakit03@gmail.com>
2025-02-05 21:25:06 +0700

    Delete images and Delete  some syntax in javascrip

--- js/script.js
+++ js/script.js
@@ -5,17 +5,3 @@ const deviceLink = document.getElementById("devices-link")
 menuIcon.addEventListener("click", () => {
     menuList.classList.toggle("hidden")
 });
-
-deviceLink.forEach(link => {
-    link.addEventListener('click', function(e){
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-
-        targetElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-});
